1722cf34edb6707cf1417c351c99d3281b240d53
bogdan-dankovskyi <bogdan.dankovskyi@gmail.com>
2020-03-08 08:16:19 +0200

    Add files via upload

--- template_find.cpp
+++ template_find.cpp
@@ -2,10 +2,63 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+
+template <typename InputIterator, typename T>
+InputIterator myFind(InputIterator itFirst, InputIterator itLast, const T& val)
+{
+	while (itFirst != itLast)
+	{
+		if (*itFirst == val)
+			return itFirst;
+		else
+			itFirst++;
+	}
+
+	return itLast;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<int> a{0,1,2,3,4,5};
+
+	if (myFind(begin(a), end(a), 3) != end(a))
+		std::cout << "found 3" << std::endl;
+	else
+		std::cout << "not found 3" << std::endl;
+
+	
+	if (myFind(begin(a), end(a), 5) != end(a))
+		std::cout << "found 5" << std::endl;
+	else
+		std::cout << "not found 5" << std::endl;
+
+	if (myFind(begin(a), end(a), 7) != end(a))
+		std::cout << "found 7" << std::endl;
+	else
+		std::cout << "not found 7" << std::endl;
+
+	std::list<std::string> b{ "a","b","c","d" };
+
+	if (myFind(begin(b), end(b), "a") != end(b))
+		std::cout << "found a" << std::endl;
+	else
+		std::cout << "not found a" << std::endl;
+
+	if (myFind(begin(b), end(b), "d") != end(b))
+		std::cout << "found d" << std::endl;
+	else
+		std::cout << "not found d" << std::endl;
+
+	if (myFind(begin(b), end(b), "e") != end(b))
+		std::cout << "found e" << std::endl;
+	else
+		std::cout << "not found e" << std::endl;
+
+    std::cout << "end main\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
